242a51836fbe339096a6b7582f75a7855e197a7b
Bhattacharya <sumantra.bhattacharya@siemens.com>
2018-09-13 01:38:40 +0530

    Assignment_11.1

---
+++ script.js
@@ -0,0 +1,10 @@
+
+//execute the function while the window is loaded
+window.onload= function(){
+//setting the background color to grey
+document.body.style.backgroundColor = "#D3D3D3";
+// changing the text color for the paragraphs
+document.getElementById("p1").style.color = "purple";
+document.getElementById("p2").style.color = "red";
+}
+
